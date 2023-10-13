74a2647ed1bffb4d222b6bb6e93a330ecfde7567
Juanruiz22 <147443872+Juanruiz22@users.noreply.github.com>
2023-10-13 04:39:08 +0200

    Update Ejercicio_10.cs

--- Ejercicio_10.cs
+++ Ejercicio_10.cs
@@ -1,15 +1,1 @@
- 
-            int num1, num2;
 
-           
-            num1 = int.Parse(Console.ReadLine());
-            num2 = int.Parse(Console.ReadLine());
-
-           
-            int suma = num1 + num2;
-
-           
-            Console.WriteLine(suma);
-        }
-    }
-}
