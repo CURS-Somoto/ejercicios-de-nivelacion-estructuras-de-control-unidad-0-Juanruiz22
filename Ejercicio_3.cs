6a152053dca2efed06a7cf7a6f041f3c4f024071
Juanruiz22 <147443872+Juanruiz22@users.noreply.github.com>
2023-10-13 04:40:56 +0200

    Update Ejercicio_3.cs
    num primo trabajo

--- Ejercicio_3.cs
+++ Ejercicio_3.cs
@@ -1,5 +1,24 @@
-/*Ejercicio 3: Numero primo
+ int numero;
 
+            Console.WriteLine("Ingrese un número: ");
+            numero = int.Parse(Console.ReadLine());
+            bool esPrimo = true;
 
-Escribe un
-programa que determine si un número dado es primo o no.*/
+            for (int i = 2; i < numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    esPrimo = false;
+                    break;
+                }
+            } 
+                    if (esPrimo)
+            {
+                Console.WriteLine("El número es primo.");
+            }
+            else
+            {
+                Console.WriteLine("El número no es primo.");
+            }
+        }
+}
