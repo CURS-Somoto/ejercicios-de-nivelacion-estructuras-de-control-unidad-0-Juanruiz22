bec336969631f1dfe01066caf8509d05f7ba2b48
Juanruiz22 <147443872+Juanruiz22@users.noreply.github.com>
2023-10-13 05:05:19 +0200

    Update Ejercicio_10.cs
    c#

--- Ejercicio_10.cs
+++ Ejercicio_10.cs
@@ -1,1 +1,24 @@
+int numero;
+
+            Console.WriteLine("Ingrese un número: ");
+            numero = int.Parse(Console.ReadLine());
+
+        
+            if (numero > 0)
+            {
+               
+                Console.WriteLine("El número es positivo.");
+            }
+            else if (numero < 0)
+            {
+                
+                Console.WriteLine("El número es negativo.");
+            }
+            else
+            {
+                Console.WriteLine("El número es cero.");
+            }
+        }
+    }
+}
 
